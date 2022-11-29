da2abd82b2ed5c874ef2623a4a6f497318f34b12
toha43272 <toha43272@outlook.com>
2022-11-29 10:28:04 +0100

    Initial Commit

---
+++ lodden/loddenApp.swift
@@ -0,0 +1,17 @@
+//
+//  loddenApp.swift
+//  lodden
+//
+//  Created by Torstein Halvorsen on 29/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct loddenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
